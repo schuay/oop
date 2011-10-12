185cb4c3ee0cbda8b6df43e11a9602192489dc2a
Jakob Gruber <jakob.gruber@gmail.com>
2011-10-12 13:37:56 +0200

    LVA, Student, LVAManager stubs

---
+++ Aufgabe1/LVA.java
@@ -0,0 +1,16 @@
+class LVA {
+    private String title;
+    private String nr;
+
+    public LVA(String nr, String title) {
+        this.title = title;
+        this.nr = nr;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getNr() {
+        return nr;
+    }
+}
---
+++ Aufgabe1/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String matrNr;
+    private String name;
+
+    public Student(String matrNr, String name) {
+        this.matrNr = matrNr;
+        this.name = name;
+    }
+
+    public String getMatrNr() {
+        return matrNr;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
